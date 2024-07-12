03b6e5beeca82801498d3fd6d8c046a293b8e5b4
xoraql <djalilhr@gmail.com>
2024-07-12 18:02:01 +0200

    redirecting

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,18 @@
-import Image from "next/image";
+"use client"
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+import Image from 'next/image';
 
 export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push('/earn');
+  }, [router]);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      test
+      Redirecting...
     </main>
   );
 }
