0327cb1b126ca31149091a9e3584cca2a5b3cbb7
alaindixon <alain.attica@adixon.fr>
2024-07-18 16:48:35 +0200

    add V3

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -2,46 +2,73 @@
 
 import MoreGraphicLinear from '@/components/graphics/MoreGraphicsLinear';
 import MoreGraphicsV2 from '@/components/graphics/MoreGraphicsV2';
+import MoreGraphicsV3 from '@/components/graphics/MoreGraphicsV3';
 import React from 'react'
+
+
+// Fonction pour générer une map de dates avec des pourcentages pour chaque semaine de l'année 2024
+function generateWeeklyData(): Map<string, number> {
+  const startDate = new Date('2024-01-01');
+  const data = new Map<string, number>();
+
+  let money = 0
+  let cnt = 52
+  let moneyStep = 300 / cnt
+  for (let week = 0; week < cnt; week++) {
+      // Calculer la date de début de chaque semaine
+      const currentDate = new Date(startDate.getTime());
+      currentDate.setDate(startDate.getDate() + week * 7);
+
+      // Formater la date en chaîne de caractères "YYYY-MM-DD"
+      const dateString = currentDate.toISOString().split('T')[0];
+
+      // Ajouter la date et le pourcentage aléatoire à la map
+      money += moneyStep
+      data.set(dateString, money + Math.random() * 30);
+  }
+
+  return data;
+}
+
 
 const Test = () => {
 
 
   //EXEMPLE 1 (GAUCHE)
   //Pourcentage 0 to 100 step 10
-  const labelsX = ["10%", "20%", "30%", "40%", "50%", "60%", "70%", "80%", "90%", "100%"];
+  // const labelsX = ["10%", "20%", "30%", "40%", "50%", "60%", "70%", "80%", "90%", "100%"];
 
-  const datasets = [
-      {
-        label: "<90% Borrow APY",
-        data: [5,6,7,8,9,10,11,12,13,45],
-        borderColor:'#1DA1F2',        
-        backgroundColor: '#1DA1F2'
-      },
-      {
-        label: ">90% Max Borrow APY",
-        data: [5,6,7,8,9,10,11,12,13,50],
-        borderColor:'#1DF2A1',        
-        backgroundColor: '#1DF2A1'
-      },      
-      {
-        label: "<90% Retail APY",
-        data: [5,7,9,11,13,15,17,19,21,55],
-        borderColor:'#A11DF2',        
-        backgroundColor: '#A11DF2'
-      },      
-      {
-        label: ">90% Max Supply APY",
-        data: [5,7,9,11,13,15,17,19,21,53],
-        borderColor:'#F2A11D',        
-        backgroundColor: '#F2A11D'
-      },            
-    ]
+  // const datasets = [
+  //     {
+  //       label: "<90% Borrow APY",
+  //       data: [5,6,7,8,9,10,11,12,13,45],
+  //       borderColor:'#1DA1F2',        
+  //       backgroundColor: '#1DA1F2'
+  //     },
+  //     {
+  //       label: ">90% Max Borrow APY",
+  //       data: [5,6,7,8,9,10,11,12,13,50],
+  //       borderColor:'#1DF2A1',        
+  //       backgroundColor: '#1DF2A1'
+  //     },      
+  //     {
+  //       label: "<90% Retail APY",
+  //       data: [5,7,9,11,13,15,17,19,21,55],
+  //       borderColor:'#A11DF2',        
+  //       backgroundColor: '#A11DF2'
+  //     },      
+  //     {
+  //       label: ">90% Max Supply APY",
+  //       data: [5,7,9,11,13,15,17,19,21,53],
+  //       borderColor:'#F2A11D',        
+  //       backgroundColor: '#F2A11D'
+  //     },            
+  //   ]
 
-  const guideLines = [
-    {"labelX":"2024-03-25", "name":"Important Date"},
-    {"labelX":"2024-04-21", "name":"Super Date"},
-  ]
+  // const guideLines = [
+  //   {"labelX":"2024-03-25", "name":"Important Date"},
+  //   {"labelX":"2024-04-21", "name":"Super Date"},
+  // ]
 
 
 //EXEMPLE 2 (DROITE)
@@ -71,10 +98,21 @@ const Test = () => {
 //max={3}
     
   
+      // Utiliser la fonction pour générer les données
+      const weeklyPercentages = generateWeeklyData();
+      const labelsX = Array.from(weeklyPercentages.keys());  
+      const datasets = [
+          {
+            label: "Data",
+            data: Array.from(weeklyPercentages.values()),
+            borderColor:'#1DA1F2',        
+            backgroundColor: '#1DA1F2'
+          },
+        ]
 
   return (
     <>
-      <div className="mockup-window border-base-300 border"><MoreGraphicsV2  datasets={datasets} labelsX={labelsX} comment='Learn MORE about ratings and how to get rated here.'></MoreGraphicsV2></div>
+      <div className="mockup-window border-base-300 border"><MoreGraphicsV3  datasets={datasets} labelsX={labelsX} total={"$100M"}></MoreGraphicsV3></div>
     </>
   )
 }
---
+++ components/graphics/MoreGraphicsV3.tsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import MoreGraphicLinear from './MoreGraphicsLinear'
+import Icon from '../FontAwesomeIcon'
+
+interface Props {  
+    labelsX: string[], // Obligatoire
+    datasets: Dataset[], // Obligatoire
+    signY?: string, // Optionnel
+    isFill?: boolean, // Optionnel
+    transparency?: number, // Optionnel
+    borderWidth?: number, // Optionnel
+    pointRadius?: number, // Optionnel
+    minY?: number, // Optionnel
+    maxY?: number, // Optionnel
+    guideLines?: any[], // Optionnel
+    comment?: string, // Optionnel
+    total?: string, // Optionnel
+  }
+
+const MoreGraphicsV3 = ({ datasets, labelsX, comment, isFill, signY, borderWidth, pointRadius, transparency, minY, maxY, guideLines, total }: Props) => {
+  return (
+    <>
+        <div className="p-2 flex justify-between items-center">                    
+            <div className="p-2">                    
+              <div className="text-xl mb-2">Total Borrow</div>                         
+              <div className='text-secondary text-3xl'>{ total }</div>                         
+            </div>
+             <div>
+              <button className="btn btn-active btn-neutral mr-5 more-text-gray">Total Borrow</button>
+              <button className="btn btn-active btn-neutral mr-5 more-text-gray">Total Supply</button>
+              <button className="btn btn-active btn-neutral mr-5 more-text-gray">Liquidity</button>
+              <button className="btn btn-active btn-neutral mr-5 more-text-gray">Utilization</button>
+              <button className="btn btn-active btn-neutral mr-5 more-text-gray">USD</button>
+              <button className="btn btn-active btn-neutral mr-5 more-text-gray">DAI</button>   
+              <div className="dropdown dropdown-end">
+                <label tabIndex={0} className="btn m-1 btn-neutral">3M <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-6 h-6 ml-2 stroke-current"> 
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path> 
+                </svg></label>
+                <ul tabIndex={0} className="dropdown-content menu p-2 shadow bg-base-200 rounded-box w-52">
+                  <li className="bg-neutral"><a>Item 1</a></li>
+                  <li className="bg-neutral"><a>Item 2</a></li>
+                  <li className="bg-neutral"><a>Item 3</a></li>
+                </ul>
+              </div>                       
+             </div>            
+        </div>
+        <div><MoreGraphicLinear datasets={datasets} labelsX={labelsX}></MoreGraphicLinear></div>
+    </>
+  )
+}
+
+export default MoreGraphicsV3
