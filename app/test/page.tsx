7249c5b502456a0934e8cbda2ae607b9db589e51
alaindixon <alain.attica@adixon.fr>
2024-07-18 15:16:15 +0200

    Graphics V1 et V2

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -7,72 +7,74 @@ import React from 'react'
 const Test = () => {
 
 
+  //EXEMPLE 1 (GAUCHE)
   //Pourcentage 0 to 100 step 10
-  // const labelsX = ["10%", "20%", "30%", "40%", "50%", "60%", "70%", "80%", "90%", "100%"];
+  const labelsX = ["10%", "20%", "30%", "40%", "50%", "60%", "70%", "80%", "90%", "100%"];
 
-  // const datasets = [
-  //     {
-  //       label: "<90% Borrow APY",
-  //       data: [5,6,7,8,9,10,11,12,13,45],
-  //       borderColor:'#1DA1F2',        
-  //       backgroundColor: '#1DA1F2'
-  //     },
-  //     {
-  //       label: ">90% Max Borrow APY",
-  //       data: [5,6,7,8,9,10,11,12,13,50],
-  //       borderColor:'#1DF2A1',        
-  //       backgroundColor: '#1DF2A1'
-  //     },      
-  //     {
-  //       label: "<90% Retail APY",
-  //       data: [5,7,9,11,13,15,17,19,21,55],
-  //       borderColor:'#A11DF2',        
-  //       backgroundColor: '#A11DF2'
-  //     },      
-  //     {
-  //       label: ">90% Max Supply APY",
-  //       data: [5,7,9,11,13,15,17,19,21,53],
-  //       borderColor:'#F2A11D',        
-  //       backgroundColor: '#F2A11D'
-  //     },            
-  //   ]
+  const datasets = [
+      {
+        label: "<90% Borrow APY",
+        data: [5,6,7,8,9,10,11,12,13,45],
+        borderColor:'#1DA1F2',        
+        backgroundColor: '#1DA1F2'
+      },
+      {
+        label: ">90% Max Borrow APY",
+        data: [5,6,7,8,9,10,11,12,13,50],
+        borderColor:'#1DF2A1',        
+        backgroundColor: '#1DF2A1'
+      },      
+      {
+        label: "<90% Retail APY",
+        data: [5,7,9,11,13,15,17,19,21,55],
+        borderColor:'#A11DF2',        
+        backgroundColor: '#A11DF2'
+      },      
+      {
+        label: ">90% Max Supply APY",
+        data: [5,7,9,11,13,15,17,19,21,53],
+        borderColor:'#F2A11D',        
+        backgroundColor: '#F2A11D'
+      },            
+    ]
 
-  // const guideLines = [
-  //   {"labelX":"2024-03-25", "name":"Important Date"},
-  //   {"labelX":"2024-04-21", "name":"Super Date"},
-  // ]
+  const guideLines = [
+    {"labelX":"2024-03-25", "name":"Important Date"},
+    {"labelX":"2024-04-21", "name":"Super Date"},
+  ]
 
 
+//EXEMPLE 2 (DROITE)
 //Pourcentage 0 to 100 step 10
-const labelsX = ["0%", "25%", "50%", "75%", "100%", "125%"];
+// const labelsX = ["0%", "25%", "50%", "75%", "100%", "125%"];
 
-const datasets = [
-    {
-      label: "Base Interest Rate",
-      data: [1,1],
-      borderColor:'#1DF2A1',        
-      backgroundColor: '#1DF2A1'      
-    },
-    {
-      label: "Credora CCC Rating",
-      data: [1,1,1,1,1.5,2],
-      borderColor:'#1DA1F2',        
-      backgroundColor: '#1DA1F2'      
-    },      
-    {
-      label: "Credora BBB+ Rating",
-      data: [1,1,1,1,2],
-      borderColor:'#A11DF2',        
-      backgroundColor: '#A11DF2'
-    },               
-   ]
-
+// const datasets = [
+//     {
+//       label: "Base Interest Rate",
+//       data: [1,1],
+//       borderColor:'#1DF2A1',        
+//       backgroundColor: '#1DF2A1'      
+//     },
+//     {
+//       label: "Credora CCC Rating",
+//       data: [1,1,1,1,1.5,2],
+//       borderColor:'#1DA1F2',        
+//       backgroundColor: '#1DA1F2'      
+//     },      
+//     {
+//       label: "Credora BBB+ Rating",
+//       data: [1,1,1,1,2],
+//       borderColor:'#A11DF2',        
+//       backgroundColor: '#A11DF2'
+//     },               
+//    ]
+//max={3}
     
   
 
   return (
     <>
-      <div className="mockup-window border-base-300 border"><MoreGraphicsV2 maxY={3} datasets={datasets} labelsX={labelsX} comment='Learn MORE about ratings and how to get rated here.'></MoreGraphicsV2></div>
+      <div className="mockup-window border-base-300 border"><MoreGraphicsV2  datasets={datasets} labelsX={labelsX} comment='Learn MORE about ratings and how to get rated here.'></MoreGraphicsV2></div>
     </>
   )
 }
