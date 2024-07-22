2e89d2753a5b3a372cb2f37d19c68446c5019c1c
alaindixon <alain.attica@adixon.fr>
2024-07-22 15:20:01 +0200

    Add format Number

---
+++ components/tools/formatNumber.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+interface Props {
+  value: number;  // Définit clairement que la prop attendue est un nombre
+}
+
+const formatNumber = (value: number): string => {
+    return value.toLocaleString('en-US', {
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2
+    });
+}
+
+const FormatNumber: React.FC<Props> = ({ value }) => {
+    // Vous pouvez ajouter une vérification ici si besoin
+    return <span>{formatNumber(value)}</span>;  // Utilisez <span> ou autre élément selon le contexte d'usage
+}
+
+export default FormatNumber;
