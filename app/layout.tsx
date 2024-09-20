e2daa6810ff97fb6750530685f7e0a03a7b23124
mmsys34 <leon@more.markets>
2024-09-20 19:41:32 +1000

    feat: add google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,23 @@ const RootLayout = ({ children }: LayoutProps) => {
   return (
     <html lang="en">
       <head>
+        {/* Google tag (gtag.js) */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-9W0VBJHBWM"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-9W0VBJHBWM', {
+                page_path: window.location.pathname
+              });
+            `,
+          }}
+        />
         <title>More Markets</title>
         <link
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"
