dc0ef9c5fab933019e1ac1de7455467774c57c5e
mmsys34 <leon@more.markets>
2024-09-30 20:18:23 +1000

    feat: update for gtag script

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import React, { ReactNode } from "react";
 import MainLayout from "@/layouts/MainLayout";
+import { GoogleAnalytics } from "@next/third-parties/google";
 import { Providers } from "./providers";
 
 import "@rainbow-me/rainbowkit/styles.css";
@@ -14,28 +15,13 @@ const RootLayout = ({ children }: LayoutProps) => {
   return (
     <html lang="en">
       <head>
-        {/* Google tag (gtag.js) */}
-        <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-9W0VBJHBWM"
-        />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-9W0VBJHBWM', {
-                page_path: window.location.pathname
-              });
-            `,
-          }}
-        />
         <title>More Markets</title>
         <link
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"
           rel="stylesheet"
         />
+        {/* Google tag (gtag.js) */}
+        <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_ID || ""} />
       </head>
       <body>
         <Providers>
