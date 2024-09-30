7f458f69ef0e1313725f6087c563552ef7a6b1ec
mmsys34 <leon@more.markets>
2024-10-01 00:29:44 +1000

    feat: update for position change in repay

--- components/modal/repay/VaultRepayPush.tsx
+++ components/modal/repay/VaultRepayPush.tsx
@@ -11,9 +11,15 @@ import { ArrowLongRightIcon } from "@heroicons/react/16/solid";
 import FormatPourcentage from "@/components/tools/formatPourcentage";
 import PositionChangeToken from "@/components/token/PositionChangeToken";
 import FormatTwoPourcentage from "@/components/tools/formatTwoPourcentage";
+import usePrice from "@/hooks/usePrice";
 import { BorrowPosition } from "@/types";
 import { contracts, MoreAction } from "@/utils/const";
-import { getTokenInfo, notifyError, delay } from "@/utils/utils";
+import {
+  getTokenInfo,
+  notifyError,
+  delay,
+  formatTokenValue,
+} from "@/utils/utils";
 import {
   getTokenAllowance,
   setTokenAllowance,
@@ -41,11 +47,27 @@ const VaultRepayPush: React.FC<Props> = ({
   const [isLoading, setIsLoading] = useState(false);
   const [hasApprove, setHasApprove] = useState(false);
 
+  const { tokenPrice: collateralPrice } = usePrice(item.inputToken.id);
+  const { tokenPrice: borrowPrice } = usePrice(item.borrowedToken.id);
+
   const borrowToken = getTokenInfo(item.borrowedToken.id);
   const collateralToken = getTokenInfo(item.inputToken.id);
   const loanToken = borrowToken.symbol;
   const repayAmount = parseUnits(amount.toString(), borrowToken.decimals);
-  // const loanAmount = formatTokenValue(item.loan, item.borrowedToken.id);
+  const loanAmount = formatTokenValue(item.loan, item.borrowedToken.id);
+
+  const collateralAmount = formatTokenValue(
+    item.collateral,
+    "",
+    collateralToken.decimals
+  );
+  const collateralUsd = collateralPrice * collateralAmount;
+  const beforeLtv =
+    collateralUsd > 0 ? (borrowPrice * loanAmount) / collateralUsd : 0;
+  const afterLtv =
+    collateralUsd > 0
+      ? (borrowPrice * (loanAmount - amount)) / collateralUsd
+      : 0;
 
   useEffect(() => {
     const initApprove = async () => {
@@ -107,84 +129,88 @@ const VaultRepayPush: React.FC<Props> = ({
   return (
     <div className="more-bg-secondary w-full rounded-[20px] modal-base">
       <div className="px-[28px] pt-[50px] pb-[30px] font-[16px]">
-      <div className="text-[24px] mb-[40px] font-semibold">Review Transaction</div>
-      <div className="flex items-center mb-[30px] font-semibold text-[20px] gap-2">
-        <ListIconToken
-          iconNames={[item.inputToken.id, item.borrowedToken.id]}
-          className="w-7 h-7"
-        />
-        <div className="text-l flex items-center'">
-          {collateralToken.symbol} / {loanToken}
+        <div className="text-[24px] mb-[40px] font-semibold">
+          Review Transaction
         </div>
-      </div>
-      <div className="relative more-bg-primary rounded-[12px] p-[20px] mb-6">
-        <TokenAmount
-          title="Repay"
-          token={item.borrowedToken.id}
-          amount={amount}
-          ltv={""}
-          totalTokenAmount={amount}
-        />
-      </div>
-
-      {/* <div className="more-bg-primary rounded-b-[5px] mt-[1px] px-8 flex flex-col gap-4 py-8">
-        <div className="text-grey">Position Change</div>
-        <div className="flex flex-row justify-between items-center">
-          <div className="text-xl">Loan</div>
-          <div className="flex  items-center gap-2">
-            <FormatPrice value={loanAmount} token={loanToken} />
+        <div className="flex items-center mb-[30px] font-semibold text-[20px] gap-2">
+          <ListIconToken
+            iconNames={[item.inputToken.id, item.borrowedToken.id]}
+            className="w-7 h-7"
+          />
+          <div className="text-l flex items-center'">
+            {collateralToken.symbol} / {loanToken}
           </div>
         </div>
-
-        <PositionChangeToken
-          title="Borrow"
-          token={loanToken}
-          value={loanAmount}
-          value2={loanAmount - amount}
-        />
-
-        <div className="flex flex-row justify-between items-center">
-          <div className="text-xl">LTV / Liquidation LTV</div>
-          <div className="flex  items-center gap-2">
-            <FormatPourcentage value={apy} />
-            <ArrowLongRightIcon className="w-4 h-4 text-grey" />
-            <FormatTwoPourcentage value={apy} />
-          </div>
-        </div>
-
-        <PositionChangeToken
-          title="Collateral liq.price"
-          token={token}
-          value={balance}
-          value2={balance}
-        /> 
-      </div> */}
-
-      <div className="pt-5 px-5 text-[16px] leading-10">
-        By confirming this transaction, you agree to the{" "}
-        <a className="underline" href="#goto">
-          Terms of Use
-        </a>{" "}
-        and the services provisions relating to the MORE Protocol Vault.
-      </div>
-      <div className="flex justify-end more-bg-primary rounded-b-[20px] px-[28px] py-[30px]">
-        <div className="mr-5">
-          <MoreButton
-            className="text-2xl py-2"
-            text="Cancel"
-            onClick={closeModal}
-            color="gray"
+        <div className="relative more-bg-primary rounded-[12px] p-[20px] mb-6">
+          <TokenAmount
+            title="Repay"
+            token={item.borrowedToken.id}
+            amount={amount}
+            ltv={""}
+            totalTokenAmount={amount}
           />
         </div>
-        <MoreButton
-          className="text-2xl py-2"
-          text="Repay"
-          disabled={isLoading}
-          onClick={handleRepay}
-          color="primary"
-        />
+
+        <div className="more-bg-primary rounded-b-[5px] mt-[1px] px-8 flex flex-col gap-4 py-8">
+          <div className="text-grey">Position Change</div>
+          <div className="flex flex-row justify-between items-center my-2">
+            <div className="text-xl">Collateral</div>
+            <div className="flex items-center gap-2">
+              <FormatPrice
+                value={collateralAmount}
+                token={collateralToken.symbol}
+              />
+            </div>
+          </div>
+          <PositionChangeToken
+            title="Borrow"
+            token={loanToken}
+            value={loanAmount}
+            value2={loanAmount - amount}
+          />
+          <div className="flex flex-row justify-between items-center my-2">
+            <div className="text-xl">LTV / Liquidation LTV</div>
+            <div className="flex items-center gap-2">
+              <FormatPourcentage value={beforeLtv} />
+              <ArrowLongRightIcon className="w-4 h-4 text-grey" />
+              <FormatTwoPourcentage value={afterLtv} />
+            </div>
+          </div>
+
+          {/* 
+          <PositionChangeToken
+            title="Collateral liq.price"
+            token={token}
+            value={balance}
+            value2={balance}
+          /> */}
+        </div>
+
+        <div className="pt-5 px-5 text-[16px] leading-10">
+          By confirming this transaction, you agree to the{" "}
+          <a className="underline" href="#goto">
+            Terms of Use
+          </a>{" "}
+          and the services provisions relating to the MORE Protocol Vault.
+        </div>
+        <div className="flex justify-end more-bg-primary rounded-b-[20px] px-[28px] py-[30px]">
+          <div className="mr-5">
+            <MoreButton
+              className="text-2xl py-2"
+              text="Cancel"
+              onClick={closeModal}
+              color="gray"
+            />
+          </div>
+          <MoreButton
+            className="text-2xl py-2"
+            text="Repay"
+            disabled={isLoading}
+            onClick={handleRepay}
+            color="primary"
+          />
+        </div>
       </div>
-    </div>
     </div>
   );
 };
--- components/tools/formatPourcentage.tsx
+++ components/tools/formatPourcentage.tsx
@@ -8,7 +8,11 @@ const FormatPourcentage: React.FC<Props> = ({ value }) => {
   // Vous pouvez ajouter une vérification ici si besoin
   return (
     <div className="flex gap-1 justify-end">
-      <div>{typeof value == "string" ? value : (value * 100).toFixed(2)}</div>
+      <div>
+        {typeof value == "string"
+          ? value
+          : ((value <= 0 ? 0 : value) * 100).toFixed(2)}
+      </div>
       <div className="text-grey">{typeof value == "string" ? "" : "%"}</div>
     </div>
   );
