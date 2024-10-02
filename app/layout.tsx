46a52f0ac5883f3f7a024cf08f9cb9067dbfdd85
mmsys34 <leon@more.markets>
2024-10-02 23:33:55 +1000

    fix: ga code

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,4 @@
-"use client";
-
-import React, { ReactNode, useEffect, useState } from "react";
+import React, { ReactNode } from "react";
 import MainLayout from "@/layouts/MainLayout";
 import { Providers } from "./providers";
 
@@ -13,37 +11,26 @@ interface LayoutProps {
 }
 
 const RootLayout = ({ children }: LayoutProps) => {
-  const [isMounted, setMount] = useState(false);
-
-  useEffect(() => {
-    setMount(true);
-  }, []);
-
   return (
     <html lang="en">
       <head>
         {/* Google tag (gtag.js) */}
-        {isMounted && (
-          <>
-            <script
-              async
-              src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
-            />
-            <script
-              dangerouslySetInnerHTML={{
-                __html: `
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=G-9W0VBJHBWM`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
-              gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}', {
+              gtag('config', 'G-9W0VBJHBWM', {
                 page_path: window.location.pathname
               });
             `,
-              }}
-            />
-          </>
-        )}
-
+          }}
+        />
         <title>More Markets</title>
         <link
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"
