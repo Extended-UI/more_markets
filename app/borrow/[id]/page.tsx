2ad8060f002cb4bbd8bc945ee9c3fa1e9505d14a
mmsys34 <leon@more.markets>
2024-12-12 11:59:17 +0700

    feat: restore the redirect when error occured

--- app/borrow/[id]/page.tsx
+++ app/borrow/[id]/page.tsx
@@ -113,8 +113,8 @@ const BorrowDetailPage: React.FC = () => {
           router.push("/borrow");
         }
       } catch (err) {
-        console.log(err);
-        // router.push("/borrow");
+        // console.log(err);
+        router.push("/borrow");
       }
     };
 
